a05013f1bd8ef3ceba244f2c5c5b15b1c7b802f2
elmaccho <106253529+elmaccho@users.noreply.github.com>
2023-02-10 22:12:58 +0100

    Add files via upload

--- infinite scroll/script.js
+++ infinite scroll/script.js
@@ -1,15 +1,31 @@
 const imgs = document.querySelectorAll('img')
 const navBtns = document.querySelectorAll('.navBtn')
+const mainGallery = document.querySelector('.mainGallery')
+const search = document.querySelector('.search')
+const watch = document.querySelector('.watch')
+const navBox = document.querySelector('.navBox')
+
+const postBox = document.createElement('div')
+const imgBox = document.createElement('img')
+const pBox = document.createElement('p')
+
+postBox.classList.add('postBox')
 
 const URL = 'https://dog.ceo/api/breeds/image/random'
 
-const test = (e) => {
-    console.log(e.target);
+
+const watchClick = () => {
+    navBox.style.left = "72%"
+    navBox.style.width = "60px"
 }
 
-for(const navBtn of navBtns){
-    navBtn.addEventListener('click', test)
+const searchClick = () => {
+    navBox.style.left = "50%"
+    navBox.style.width = "67px"
 }
+
+
+
 
 for(const img of imgs){
     for(let i=0; i<imgs.length; i++){
@@ -19,3 +35,56 @@ for(const img of imgs){
             .catch(err => console.error(err))
     }
 }
+
+// const createBox = () => {
+//     const boxDiv = document.createElement('div')
+//     const boxSett = boxDiv.createElement('div')
+//     const boxImg = document.createElement('img')
+//     const boxP = document.createElement('p')
+//     const boxI = document.createElement('i')
+
+    
+//     boxDiv.classList.add('postBox')
+//     boxSett.classList.add('boxSett')
+//     boxI.classList.add('fa-solid fa-ellipsis')
+
+//     boxDiv.append(boxImg)
+//     boxSett.append(boxP)
+//     boxSett.append(boxI)
+
+// }
+
+
+const createPost = () => {
+    mainGallery.append(postBox)
+    postBox.append(imgBox)
+
+}
+
+
+const createImage = () => {
+    for(const img of imgs){
+        for(let i=0; i<imgs.length; i++){
+            fetch(URL)
+                .then(res => res.json())
+                .then(data => img.setAttribute('src', data.message))
+                .catch(err => console.error(err))
+        }
+    }
+}
+
+window.addEventListener('scroll', () => {
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	
+	console.log( { scrollTop, scrollHeight, clientHeight });
+	
+	if(clientHeight + scrollTop >= scrollHeight - 5) {
+		console.log('nowe posty');
+        createPost()
+	}
+})
+
+
+
+watch.addEventListener('click', watchClick)
+search.addEventListener('click', searchClick)
