b82d96c362070150241a80724740f4e61eb8206d
elmaccho <106253529+elmaccho@users.noreply.github.com>
2023-02-11 00:38:18 +0100

    Add files via upload

--- infinite scroll/script.js
+++ infinite scroll/script.js
@@ -2,10 +2,10 @@ const pCounters = document.querySelectorAll('.count')
 const imgs = document.querySelectorAll('img')
 const search = document.querySelector('.search')
 const main = document.querySelector('.main')
+const loadingBox = document.querySelector('.loadingBox')
+
 
 const createPost = () => {
-    
-    
     const postBox = document.createElement('div')
     const postImg = document.createElement('img')
     const postInfo = document.createElement('div')
@@ -38,19 +38,46 @@ const createPost = () => {
     </button>`
     postComment.innerHTML = `<i class="fa-regular fa-user"></i> <input type="text" name="" id="" placeholder="Dodaj komentarz...">`
     
-    
-    for(let i=0; i<pCounters.length; i++){
-        for(const counter of pCounters){
-            let likes = Math.floor(Math.random() * 10000)
-            counter.textContent = `${likes} polubień`
-        }
-    }
+
 
     let randId = Math.floor(Math.random()*1000+1)
     postImg.setAttribute('src', `https://picsum.photos/340/250?random=${randId}`)
 
     main.append(postBox)
-
 }
 
-search.addEventListener('click', createPost)
+for(let i=0; i<pCounters.length; i++){
+    for(const counter of pCounters){
+        let likes = Math.floor(Math.random() * 10000)
+        counter.textContent = `${likes} polubień`
+    }
+}
+
+const loadingDots = () => {
+    document.body.style.overflow = "hidden"
+    loadingBox.classList.add("show")
+}
+
+window.addEventListener('scroll', () => {
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	
+	console.log( { scrollTop, scrollHeight, clientHeight });
+	
+	if(clientHeight + scrollTop >= scrollHeight - 5) {
+		// show the loading animation
+
+
+        loadingDots()
+        main.style.filter = "grayscale(90%) blur(3px)"
+
+            setTimeout(() => {
+                createPost()
+                createPost()
+
+                document.body.style.overflow = "scroll"
+                loadingBox.classList.remove("show")
+                main.style.filter = "grayscale(0) blur(0)"
+            }, 1000);
+
+	}
+})
